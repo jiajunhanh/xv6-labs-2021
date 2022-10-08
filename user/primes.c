47f25f70289364dcd08ede5ae94ccdcdee0b3ee7
Jiajun Han <jiajunhanh@gmail.com>
2022-10-08 18:00:47 +0900

    code formatting

--- user/primes.c
+++ user/primes.c
@@ -3,41 +3,41 @@
 #include "user/user.h"
 
 int main() {
-    int rd;
-    int p[2];
-    int num;
-    int prime;
-    pipe(p);
+  int rd;
+  int p[2];
+  int num;
+  int prime;
+  pipe(p);
 
-    if (!fork()) {
-        while (1) {
-            rd = p[0];
-            close(p[1]);
-            if (read(rd, &prime, 4) == 4) {
-                printf("prime %d\n", prime);
-            } else {
-                close(rd);
-                break;
-            }
-            pipe(p);
-            if (!fork()) continue;
-            while (read(rd, &num, 4) == 4) {
-                if (num % prime) write(p[1], &num, 4);
-            }
-            break;
-        }
+  if (!fork()) {
+    while (1) {
+      rd = p[0];
+      close(p[1]);
+      if (read(rd, &prime, 4) == 4) {
+        printf("prime %d\n", prime);
+      } else {
         close(rd);
-        close(p[0]);
-        exit(0);
+        break;
+      }
+      pipe(p);
+      if (!fork()) continue;
+      while (read(rd, &num, 4) == 4) {
+        if (num % prime) write(p[1], &num, 4);
+      }
+      break;
     }
-
-    for (int i = 2; i <= 35; ++i) {
-        write(p[1], &i, 4);
-    }
+    close(rd);
     close(p[0]);
-    close(p[1]);
-    while (wait(0) != -1);
-    sleep(1);
-
     exit(0);
+  }
+
+  for (int i = 2; i <= 35; ++i) {
+    write(p[1], &i, 4);
+  }
+  close(p[0]);
+  close(p[1]);
+  while (wait(0) != -1);
+  sleep(1);
+
+  exit(0);
 }
--- user/sleep.c
+++ user/sleep.c
@@ -3,22 +3,21 @@
 #include "user/user.h"
 
 int
-main(int argc, char *argv[])
-{
-    if(argc != 2){
-        fprintf(2, "usage: sleep ticks\n");
-        exit(1);
+main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(2, "usage: sleep ticks\n");
+    exit(1);
+  }
+
+  for (char *p = argv[1]; *p != 0; ++p) {
+    if (*p < '0' || '9' < *p) {
+      fprintf(2, "sleep: only support integer\n");
+      exit(1);
     }
+  }
 
-    for (char* p = argv[1]; *p != 0; ++p) {
-        if (*p < '0' || '9' < *p) {
-            fprintf(2, "sleep: only support integer\n");
-            exit(1);
-        }
-    }
+  int n = atoi(argv[1]);
+  sleep(n);
 
-    int n = atoi(argv[1]);
-    sleep(n);
-
-    exit(0);
+  exit(0);
 }
